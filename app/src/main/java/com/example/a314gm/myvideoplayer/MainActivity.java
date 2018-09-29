6ac3fdd9116c7b3aff6e370ccd86d139869fd854
314GM <1179730222@qq.com>
2018-09-29 21:55:05 +0800

    测试更新

--- app/src/main/java/com/example/a314gm/myvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/a314gm/myvideoplayer/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.a314gm.myvideoplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.a314gm.myvideoplayer.videoInfo.VideoInfo;
+
 public class MainActivity extends AppCompatActivity {
+    private VideoPlayView mVideoPlayView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoPlayView.findViewById(R.id.VideoPlayView);
+        mVideoPlayView.startPlayVideo(new VideoInfo(){
+
+            private String VideoTitle = "震惊！Android只用这样学习，工资上20k!";
+            private String VideoPath = "";
+
+
+            @Override
+            public String getVideoTitle() {
+                return VideoTitle;
+            }
+
+            @Override
+            public String getVideoPath() {
+                return VideoPath;
+            }
+        });
     }
 }
